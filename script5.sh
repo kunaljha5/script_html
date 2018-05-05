f6034cfda31102e27fc59dfdd2cfbb0f5a58d4d2
Kunal <kunaljha5@gmail.com>
2018-05-05 19:21:17 +0530

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-scp -i "/var/lib/jenkins/secrets/learning1.pem" ./* ubuntu@172.31.64.64:./
-scp -i "/var/lib/jenkins/secrets/learning1.pem" ./* ubuntu@172.31.66.47:./
-scp -i "/var/lib/jenkins/secrets/learning1.pem" ./* ubuntu@172.31.44.198:./
+scp -i "/var/lib/jenkins/secrets/learning1.pem" ./* ubuntu@172.31.76.181:./
+
 
